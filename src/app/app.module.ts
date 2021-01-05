79f25aa880b2ec2a3fa9b038b50d339673b3b330
Felipe <felipea.97@hotmail.com>
2021-01-04 21:02:09 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+/* import { RouterModule, Routes } from '@angular/router'; */
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+
+/* const routes: Routes = [
+  { path: 'header', component: HeaderComponent }
+]; */
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule

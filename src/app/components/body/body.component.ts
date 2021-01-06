eb3dfac6ec9720f48d4a793fc7411643fabeee3d
Felipe <felipea.97@hotmail.com>
2021-01-06 09:24:36 -0500

    Fin módulo 2

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
+  bandera = true;
+
+  frase: any = {
+    msj: "Un gran poder, requiere una gran responsabilidad",
+    aut: "Ben Parker"
+  };
+
+  personajes: string[] = ['Spiderman', 'Venom',  'Dr. Octopus']
+
   constructor() { }
 
   ngOnInit(): void {
